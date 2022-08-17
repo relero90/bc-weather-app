d9171149e8b0d999fd88594ad6aa490a9ed61466
relero90 <relero90@gmail.com>
2022-08-16 18:55:51 -0600

    built logic for capuring user city inputs, saving to localStorage, and appending buttons for old searches

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,36 @@
+var searchBtn = $("#search-btn");
 var savedCitiesDiv = $("#saved-cities");
+var savedCities = [];
 var currentCityHeader = $("#cityname-date");
 var todaysTemp = $("#cur-temp");
 var todaysWind = $("#cur-wind");
 var todaysHumidity = $("#cur-humidity");
 var todaysUV = $("#cur-UV");
-// city input needs to be a string when it goes into requestUrl
 var userCityInput = $("#city-input");
+// cities with spaces break the API call
 var selectedCity = "";
 var APIKey = "ad917aaaa96b4e27d19270a99cf00379";
 
-console.log(userCityInput.value);
+function captureNewCity() {
+  selectedCity = userCityInput.val();
+  console.log(selectedCity);
+
+  // overwrites self each time
+  savedCities.push(selectedCity);
+  var storedStringInput = JSON.stringify(savedCities);
+  localStorage.setItem("savedCitiesString", storedStringInput);
+
+  // create button element
+  var savedCity = document.createElement("button");
+  // add text = selected city
+  savedCity.textContent = selectedCity;
+  // add class = selected city
+  savedCity.classList.add(selectedCity);
+  // Append button to savedCitiesDiv
+  savedCitiesDiv.append(savedCity);
+}
+
+function renderSavedCity() {}
 
 function getWeatherData() {
   // API url to get current weather information
@@ -28,3 +49,12 @@ function getWeatherData() {
     });
 }
 // getWeatherData();
+
+searchBtn.on("click", function (event) {
+  console.log(userCityInput.val());
+  event.preventDefault();
+
+  if (userCityInput.val() !== null) {
+    captureNewCity();
+  }
+});
