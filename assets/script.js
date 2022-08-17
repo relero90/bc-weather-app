a43e172a954154b5e6a237f3c6255c1776501d98
relero90 <relero90@gmail.com>
2022-08-16 18:07:21 -0600

    API request URL now functioning as expected

--- assets/script.js
+++ assets/script.js
@@ -4,17 +4,27 @@ var todaysTemp = $("#cur-temp");
 var todaysWind = $("#cur-wind");
 var todaysHumidity = $("#cur-humidity");
 var todaysUV = $("#cur-UV");
+// city input needs to be a string when it goes into requestUrl
 var userCityInput = $("#city-input");
+var selectedCity = "";
 var APIKey = "ad917aaaa96b4e27d19270a99cf00379";
 
-function getWeatherData() {
-  // Insert the API url to get weather information
-  // Returning unauthorized - OpenWeather email says it needs a couple hours to activate my API keys
-  var requestUrl = "https://api.openweathermap.org/data/3.0/onecall?appid={}";
+console.log(userCityInput.value);
 
-  fetch(requestUrl).then(function (response) {
-    console.log(response);
-    return response.json();
-  });
+function getWeatherData() {
+  // API url to get current weather information
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    selectedCity +
+    "&appid=" +
+    APIKey;
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
-getWeatherData();
+// getWeatherData();
