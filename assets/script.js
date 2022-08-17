47718ba6611cc68de9fe3764722b16dabda7ffd0
relero90 <relero90@gmail.com>
2022-08-16 19:25:58 -0600

    built logic for populating stored searches as buttons at startup

--- assets/script.js
+++ assets/script.js
@@ -11,26 +11,38 @@ var userCityInput = $("#city-input");
 var selectedCity = "";
 var APIKey = "ad917aaaa96b4e27d19270a99cf00379";
 
-function captureNewCity() {
+// on page load, generate buttons for saved cities from stored searches in local storage
+function renderSavedCities() {
+  var pulledCities = JSON.parse(localStorage.getItem("savedCitiesString"));
+  if (pulledCities !== null) {
+    // For each item in the pulledCities array,
+    for (var i = 0; i < pulledCities.length; i++) {
+      var savedCity = document.createElement("button");
+      savedCity.textContent = pulledCities[i];
+      savedCity.classList.add(pulledCities[i]);
+      savedCity.setAttribute("data-index", i);
+      savedCitiesDiv.append(savedCity);
+    }
+  }
+}
+renderSavedCities();
+
+// when user enters a new city and clicks "search", saves to local storage and appends a button for that city
+function saveNewCity() {
   selectedCity = userCityInput.val();
   console.log(selectedCity);
-
-  // overwrites self each time
   savedCities.push(selectedCity);
+  console.log(savedCities);
+
   var storedStringInput = JSON.stringify(savedCities);
   localStorage.setItem("savedCitiesString", storedStringInput);
-
-  // create button element
+  // create button element, add text, class, and append below search
   var savedCity = document.createElement("button");
-  // add text = selected city
   savedCity.textContent = selectedCity;
-  // add class = selected city
   savedCity.classList.add(selectedCity);
-  // Append button to savedCitiesDiv
   savedCitiesDiv.append(savedCity);
+  // userCityInput.textContent = "";
 }
-
-function renderSavedCity() {}
 
 function getWeatherData() {
   // API url to get current weather information
@@ -51,10 +63,8 @@ function getWeatherData() {
 // getWeatherData();
 
 searchBtn.on("click", function (event) {
-  console.log(userCityInput.val());
   event.preventDefault();
-
   if (userCityInput.val() !== null) {
-    captureNewCity();
+    saveNewCity();
   }
 });
